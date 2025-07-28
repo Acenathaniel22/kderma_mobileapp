b1d755dea1ceed9a21691c553a01aa71bcfb2d71
Acenathaniel22 <sinagacenathaniel@gmail.com>
2025-07-28 09:32:56 +0800

    ayusinmosignupteluts

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (context) => HomePage(),
         '/dashboard': (context) => Dashboard(),
